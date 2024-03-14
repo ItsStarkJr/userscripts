1b651cf4bb8fca07e12c1e8ae3ab4fe3714c126b
ItsStarkJr <97918042+ItsStarkJr@users.noreply.github.com>
2024-03-14 01:29:25 +0100

    Further set up.

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,25 @@
 console.log("bobo");
+
+const scriptButton = document.getElementById("scripts-button");
+const installButton = document.getElementById("install-button");
+const scriptsContainer = document.getElementById("center-container-scripts");
+const installContainer = document.getElementById("center-container-install");
+
+scriptButton.addEventListener("click", function () {
+  scriptButton.classList.add("top-button-active");
+  installButton.classList.remove("top-button-active");
+  installContainer.classList.add("hide");
+  scriptsContainer.classList.remove("hide");
+});
+
+installButton.addEventListener("click", function () {
+  installButton.classList.add("top-button-active");
+  scriptButton.classList.remove("top-button-active");
+  scriptsContainer.classList.add("hide");
+  installContainer.classList.remove("hide");
+});
+
+document.getElementById("light-dark-button").addEventListener("click", () => {
+  // document.body.classList.toggle("dark");
+  console.log("liiiiiiiiiiiiiiight");
+});
